8a33a21ab7abb5cf0320b420568589cdd3a058e8
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-10-29 20:45:21 +0700

    Create Backend Project

---
+++ src/main/java/com/mystogan/MysHotel/MysHotelApplication.java
@@ -0,0 +1,13 @@
+package com.mystogan.MysHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mystogan/MysHotel/MysHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystogan.MysHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
