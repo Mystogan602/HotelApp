ea4b195e5af689c4e1446139623545444465c799
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-11-01 15:29:59 +0700

    Security Filter Chain

--- src/main/java/com/mystogan/MysHotel/service/CustomUserDetailsService.java
+++ src/main/java/com/mystogan/MysHotel/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.mystogan.MysHotel.service;
 
 import com.mystogan.MysHotel.exception.OurException;
 import com.mystogan.MysHotel.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,9 +10,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
         return userRepository.findByEmail(username).orElseThrow(()-> new OurException("Username/Email not found"));
