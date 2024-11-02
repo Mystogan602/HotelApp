0f72b8dbbdf659c7a404d552802c94cdadd08b8b
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-11-02 16:41:38 +0700

    Configure S3 Bucket

--- src/main/java/com/mystogan/MysHotel/service/CustomUserDetailsService.java
+++ src/main/java/com/mystogan/MysHotel/service/CustomUserDetailsService.java
@@ -3,7 +3,6 @@ package com.mystogan.MysHotel.service;
 import com.mystogan.MysHotel.exception.OurException;
 import com.mystogan.MysHotel.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
