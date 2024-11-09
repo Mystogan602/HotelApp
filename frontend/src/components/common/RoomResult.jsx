0187b4e643c591da9d9881bfa4ae0054bafb5202
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-11-09 16:51:13 +0700

    Room Result Component

---
+++ frontend/src/components/common/RoomResult.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import ApiService from '../../service/ApiService'
+
+const RoomResult = ({ roomSearchResult }) => {
+    const navigate = useNavigate();
+    const isAdmin = ApiService.isAdmin();
+    return (
+        <section className='room-result'>
+            {roomSearchResult && roomSearchResult.length > 0 && (
+                <div className='room-list'>
+                    {roomSearchResult.map((room) => (
+                        <div className='room-item' key={room.id}>
+                            <img className='room-image' src={room.image} alt={room.roomType} />
+                            <div className='room-details'>
+                                <h3>{room.roomType}</h3>
+                                <p>Price: ${room.price}</p>
+                                <p>Description: {room.description}</p>
+                            </div>
+
+                            <div className='book-now-div'>
+                                {isAdmin ? (
+                                    <button className='edit-room-btn'
+                                        onClick={() => navigate(`/admin/edit-room/${room.id}`)}>
+                                        Edit room
+                                    </button>
+                                ) : (
+                                    <button className='book-now-btn'
+                                        onClick={() => navigate(`/room-details-book/${room.id}`)}>
+                                        View/Book now
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </section>
+    )
+}
+
+export default RoomResult
