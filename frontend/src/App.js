2f209b5e5d6f16feb505b5b4a645cfc4ca675124
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-11-10 22:48:35 +0700

    add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/common/Navbar/Navbar';
 import Footer from './components/common/Footer/Footer';
 import HomePage from './components/home/HomePage';
-
+import AllRoomsPage from './components/booking_rooms/AllRoomsPage/AllRoomsPage';
+import FindBookingPage from './components/booking_rooms/FindBookingPage/FindBookingPage';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,8 @@ function App() {
       <div className='content'>
         <Routes>
           <Route path="/home" element={<HomePage />} />
+          <Route path="/room" element={<AllRoomsPage />} />
+          <Route path='/find-booking' element={<FindBookingPage />} />
         </Routes>
 
       </div>
