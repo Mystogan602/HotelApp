7c0af76ffaa420e3cba860ba2f227dc547a9fe6e
Phạm Minh Tiến <phminhtien136@gmail.com>
2024-11-10 16:54:42 +0700

    rename activeclassname

--- frontend/src/components/common/Navbar/Navbar.jsx
+++ frontend/src/components/common/Navbar/Navbar.jsx
@@ -23,13 +23,13 @@ const Navbar = () => {
                 <NavLink to="/home">Mys Hotel</NavLink>
             </div>
             <ul className='navbar-ul'>
-                <li><NavLink to="/home" activeClass="active">Home</NavLink></li>
-                <li><NavLink to="/rooms" activeClass="active">Rooms</NavLink></li>
-                <li><NavLink to="/find-booking" activeClass="active">Find Booking</NavLink></li>
-                {isUser && <li><NavLink to="/profile" activeClass="active">Profile</NavLink></li>}
-                {isAdmin && <li><NavLink to="/admin" activeClass="active">Admin</NavLink></li>}
-                {!isAuthenticated && <li><NavLink to="/login" activeClass="active">Login</NavLink></li>}
-                {!isAuthenticated && <li><NavLink to="/register" activeClass="active">Register</NavLink></li>}
+                <li><NavLink to="/home" activeclassname="active">Home</NavLink></li>
+                <li><NavLink to="/rooms" activeclassname="active">Rooms</NavLink></li>
+                <li><NavLink to="/find-booking" activeclassname="active">Find Booking</NavLink></li>
+                {isUser && <li><NavLink to="/profile" activeclassname="active">Profile</NavLink></li>}
+                {isAdmin && <li><NavLink to="/admin" activeclassname="active">Admin</NavLink></li>}
+                {!isAuthenticated && <li><NavLink to="/login" activeclassname="active">Login</NavLink></li>}
+                {!isAuthenticated && <li><NavLink to="/register" activeclassname="active">Register</NavLink></li>}
                 {isAuthenticated && <li onClick={handleLogout}>Logout</li>}
             </ul>
         </nav>
